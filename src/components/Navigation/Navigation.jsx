5a47cf1249b4e7b8d21a95baf74da206acc60af0
Denys <Melnik.Denys99@gmail.com>
2024-12-21 20:44:13 +0200

    logo updated

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import { NavLink } from "react-router-dom";
 import { Container } from "../Container/Container.jsx";
 import css from "./Navigation.module.css";
+import logo from '../../assets/icons/logo.svg'
 
 const Navigation = () => {
   return (
@@ -9,9 +10,7 @@ const Navigation = () => {
       <header className={css.header}>
         <Container>
           <div className={css.navContainer}>
-            <svg width={136} height={16}>
-                <use href="/src/assets/icons/sprite.svg#Logo"></use>
-            </svg>
+          <img src={logo} alt="logo" />
             <nav className={css.nav}>
               <NavLink
                 className={({ isActive }) =>
